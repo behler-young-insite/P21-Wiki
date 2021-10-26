08c389c7a791de955d7139a25c281ce72be98a71
Derek DenHartigh <derek.denhartigh@b-y.com>
2021-10-26 11:57:23 -0400

    First!

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# What is this?
+
+This is simply an open source wiki/living document for us to document any issues we run into and quickly access them, feel free to pull the code, add your experience, and push it up.
---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+// const matrixStyles = ["color: lime", "background-color: black", "padding: 5px", "border: solid lime 3px", "border-radius: 5px;"];
+const matrixStyles = "color: lime; background-color: black; padding: 5px; border: solid lime 3px; border-radius: 5px;"
+console.log("%cWelcome to the Matrix", matrixStyles);
