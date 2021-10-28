ee84fc83389cfec45a2aff4e73443b35899affb1
Derek DenHartigh <derek.denhartigh@b-y.com>
2021-10-28 09:41:06 -0400

    added some api slowness solutions and Felipe to Authors List

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 // const matrixStyles = ["color: lime", "background-color: black", "padding: 5px", "border: solid lime 3px", "border-radius: 5px;"];
 const matrixStyles = "color: lime; background-color: black; padding: 5px; border: solid lime 3px; border-radius: 5px;"
 console.log("%cWelcome to the Matrix", matrixStyles);
+
+// maybe down the line if this grows we'll make it a more impressive/complex page using react or something
